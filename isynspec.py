9130451b6b23f06c87285d80c4db76e008a6ceec
Sriram K <sriram@kaspar.iiap.res.in>
2017-05-02 14:04:09 +0530

    Input format changed. All fort.55 parameters now read from fort.55 instead of some from aeqw.in. Corrected error in INLIN variable names. Added RANGE and EPSILON to parameters in aeqw.in
    Committer: Sriram K <sriram@kaspar.iiap.res.in>
    	modified:   autoeqw.py
    	modified:   isynspec.py

--- isynspec.py
+++ isynspec.py
@@ -24,15 +24,15 @@ class INLIN:
             self.QL = float(line[35:39])
             self.EXCU = float(line[39:51])
             self.QU = float(line[51:55])
-            self.GS = float(line[55:63])
-            self.GW = float(line[63:70])
-            self.INEXT = float(line[70:77])
+            self.AGAM = float(line[55:63])
+            self.GS = float(line[63:70])
+            self.GW = float(line[70:77])
             self.remainder = line[79:]
         except Exception as e:
             print e
             raise InvalidInput(INLINstr)
     def __str__(self):
-        return '{0:>10.4f}{1:>3d}.{2:0>2d}{3:>7.3f}{4:>12.3f}{5:>4.1f}{6:>12.3f}{7:>4.1f} {8:>7.2f}{9:>7.2f}{10:>7.2f} 0'.format(self.ALAM, self.Z, self.Q, self.GF, self.EXCL, self.QL, self.EXCU, self.QU, self.GS, self.GW, self.INEXT)
+        return '{0:>10.4f}{1:>3d}.{2:0>2d}{3:>7.3f}{4:>12.3f}{5:>4.1f}{6:>12.3f}{7:>4.1f} {8:>7.2f}{9:>7.2f}{10:>7.2f} 0'.format(self.ALAM, self.Z, self.Q, self.GF, self.EXCL, self.QL, self.EXCU, self.QU, self.AGAM, self.GS, self.GW)
     def __cmp__(self,other):
         return self.ALAM - other.ALAM
         
@@ -48,7 +48,8 @@ class ISynspec:
     IOPHLI = 0
     IFREQ, INLTE, ICONTL, INLIST, IFHE2 = 1, 1, 0, 0, 0
     IHYDPR, IHE1PR, IHE2PR = 0, 0, 0
-    ALAM0, ALAM1, CUTOF0, CUTOFS, RELOP, SPACE = 2000.0, 5000.0, 15, 50, 0.000000000100, 0.01
+    CUTOF0, RELOP, SPACE = 15, 0.000000000100, 0.01
+    ALAM0, ALAM1, CUTOFS = 2000.0, 5000.0, 50
     VTB = 13.0
     # fort.19
     LINELIST = []
@@ -63,7 +64,7 @@ class ISynspec:
                '{16:.1f} {17:.1f} {18:d} {19:d} {20:.12f} {21:f}\n'
                '{22:f}\n')
     def __init__(self):
-        pass
+        self.read55()
     # Test if the Temperature and Gravity are same in both the model and concentration file
     def TestTG(self):
         with open('fort.8') as f:
@@ -90,7 +91,7 @@ class ISynspec:
             f.write('{0:d}\n'.format(len(self.ABUNDANCES)))
             for ABUN in self.ABUNDANCES:
                 f.write('{0:d} {1:e}\n'.format(ABUN[0],ABUN[1]).replace('e',''))
-    # Methods to read from output file
+    # Methods to read from output file.
     def read16(self):
         self.EQW = []
         with open('fort.16') as f:
@@ -98,6 +99,24 @@ class ISynspec:
                 tokens = line.split()
                 self.EQW.append( ( (float(tokens[0]),float(tokens[1])), float(tokens[2]) ) )
         return self.EQW
+    # Reading fort.55 for values of all parameter.
+    def read55(self):
+        with open('fort.55') as f:
+            # Line 1
+            self.IMODE, self.IDSTD, self.IPRIN = [int(i) for i in f.readline().split()]
+            # Line 2
+            self.INMOD, self.INTRPL, self.ICHANG, self.ICHEMC = [int(i) for i in f.readline().split()]
+            # Line 3
+            self.IOPHLI = int(f.readline().strip())
+            # Line 4
+            self.IFREQ, self.INLTE, self.ICONTL, self.INLIST, self.IFHE2 = [int(i) for i in f.readline().split()]
+            # Line 5
+            self.IHYDPR, self.IHE1PR, self.IHE2PR = [int(i) for i in f.readline().split()]
+            # Line 6
+            tokens = f.readline().split()
+            self.CUTOF0, self.RELOP, self.SPACE = int(tokens[2]), float(tokens[4]), float(tokens[5])
+            # Line 7
+            self.VTB = float(f.readline().strip())
     # Running the SYNSPEC program. self.runs is a counter that keeps track of number of runs.
     def run(self):
         self.runs += 1
