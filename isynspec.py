797e4d690b642f3820ed977ab2cda5bc7170901b
Sriram K <sriram.k@iiap.res.in>
2017-07-19 19:52:46 +0530

    Fixed test to check temp/g Test was to check if temp/g are same in both model and model input. Change is to accomodate for lack of information in model sometimes. Now if information is unavailable in the model file the program will only print a warning on screen and not in output.

--- isynspec.py
+++ isynspec.py
@@ -74,17 +74,23 @@ class ISynspec:
     def __init__(self):
         self.read55()
     # Test if the Temperature and Gravity are same in both the model and concentration file
+    # An error was found in this method of reading fort.8. It seems not all fort.8 files have this information.
     def TestTG(self):
         logger.debug(" Checking to see if T and log g are consistent between files.")
-        with open('fort.8') as f:
-            tokens = f.readline().split()
-            self.TEMP = float(tokens[2])
-            self.LOGG = float(tokens[3])
         with open(self.modelFN) as f:
             tokens = f.readline().split()
-            TEMP = float(tokens[0])
-            LOGG = float(tokens[1])
-        return TEMP == self.TEMP and LOGG == self.LOGG
+            self.TEMP = float(tokens[0])
+            self.LOGG = float(tokens[1])
+        try:
+            with open('fort.8') as f:
+                tokens = f.readline().split()
+                self.TEMP = float(tokens[2])
+                self.LOGG = float(tokens[3])
+        except IndexError:
+            logger.warning("Unable to read temperature and gravity information in fort.8")
+            return True # Reurning True although there is an error since there is no mismatch. Only absence of data.
+        else:
+            return TEMP == self.TEMP and LOGG == self.LOGG
 
     # Methods to write to input files.
     def write55(self):
